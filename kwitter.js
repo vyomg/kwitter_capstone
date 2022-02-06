a674216b3b03ccd9c07cf3a74db3fbfa506e2cec
vyomg <83865210+vyomg@users.noreply.github.com>
2022-02-06 14:56:59 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAkqSZQjGQ0d3spTWX0rwA1iX10pDhiSs8",
+    authDomain: "kwitter-4741b.firebaseapp.com",
+    databaseURL: "https://kwitter-4741b-default-rtdb.firebaseio.com",
+    projectId: "kwitter-4741b",
+    storageBucket: "kwitter-4741b.appspot.com",
+    messagingSenderId: "370924038397",
+    appId: "1:370924038397:web:61f6ac8f01c2971ec73694",
+    measurementId: "G-8BPFL78YMR"
+  };
---
+++ kwitter_room.js
@@ -0,0 +1,48 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+function addRoom()
+{
+      room_name = document.getElementById("room_name").Value;
+      firebase.database().ref("/").child(room_name).update ({
+            purpose : "adding room name"
+      });
+
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      console.log("Room Name  - " + Room_names);
+      row = "<div id='room_name' class="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>"
+      document.getElementById("output").innerHTML += row;
+      });});}
+getData();
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+function logout() {
+
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+
+      window.location = "kwitter.html";
+}
+
+function send() {
+
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name:user_name,
+            massage:msg,
+            like:0
+      })
+
+      document.getElementById("msg").value = "";
+}
